d9fb26cd7709bcaf9e88a71b10961aad6d0e07b1
Joseph Scott <jbs198585@gmail.com>
2019-04-02 06:28:45 -0400

    do while

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -9,14 +9,11 @@ public class Main {
             count++;
         }
 
-	    count = 0;
-	    while(true) {
-	        if(count == 5) {
-	            break;
-            }
-            System.out.println("Count value is " + count);
-	        count++;
-        }
+	    count = 1;
+	    do {
+            System.out.println("Count value was " + count);
+            count++;
+        } while(count != 6);
 
     }
 }
