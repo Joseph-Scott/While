33339eb1c36bd0a0cbe4639fcf5332f5aa79dc1a
Joseph Scott <jbs198585@gmail.com>
2019-04-02 06:36:38 -0400

    challenge question

--- src/com/joescott/Main.java
+++ src/com/joescott/Main.java
@@ -3,17 +3,23 @@ package com.joescott;
 public class Main {
 
     public static void main(String[] args) {
-	    int count = 0;
-	    while(count != 5) {
-            System.out.println("Count value is " + count);
-            count++;
-        }
-
-	    count = 1;
-	    do {
-            System.out.println("Count value was " + count);
-            count++;
-        } while(count != 6);
+//	    int count = 0;
+//	    while(count != 5) {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+//
+//	    count = 1;
+//	    do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//        } while(count != 6);
 
     }
+
+    // Create a method called isEvenNumber that takes a parameter of type int
+    // Its purpose is to determine if the argument passed to the method is
+    // an even number or not.
+    // return true if an even number, otherwise return false;
+
 }
